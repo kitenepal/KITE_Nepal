d11f1a7cb28cfdbe50933296f8fab03ef8ca3c9d
NidanStha <nidanshrestha@gmail.com>
2018-06-21 14:01:28 +0545

    floating modal

--- js/header.js
+++ js/header.js
@@ -1,8 +1,9 @@
-$(window).scroll(()=>{
+	$(window).scroll(()=>{
 	var windowTop = $(window).scrollTop();
 	windowTop > 70 ? $('nav').removeClass('trans-back') : $('nav').addClass('trans-back');
 });
 
+/*
 $(function(){
   $('.group').on('mouseover',function(){
     $('.main-image').css({'background':'url(img/web.jpg)','background-position':'top center','background-size':'cover'});
@@ -10,4 +11,4 @@ $(function(){
   $('.group').on('mouseleave',function(){
     $('.main-image').css({'background':'','background-position':'','background-size':''});
   });
-});
+});*/
