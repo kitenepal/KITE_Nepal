ce31dd188c3f146a566f32508444fc56b0bdfc56
NidanStha <nidanshrestha@gmail.com>
2018-06-24 17:47:35 +0545

    added smooth scrolling js

--- js/header.js
+++ js/header.js
@@ -1,7 +1,18 @@
 $(window).scroll(()=>{
 	var windowTop = $(window).scrollTop();
 	windowTop > 70 ? $('nav').removeClass('trans-back') : $('nav').addClass('trans-back');
+
+
 });
+
+
+$('nav ul>li>a[href*="#"]').on('click', function(e){
+    $('html,body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 65
+    },500);
+    e.preventDefault();
+});
+
 
 /*
 $(function(){
